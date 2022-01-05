b6ba87081bcade4f2d7b6cd7533b8626fbde4805
Pavel Stepanov <pavelstepanov@vivaldi.net>
2022-01-05 09:04:16 +0600

    normalize time & date, log open and close entries

--- src/log.lisp
+++ src/log.lisp
@@ -3,17 +3,25 @@
   (:export
    #:*log*
    #:log-message
-   #:log-time))
+   #:log-timestamp
+   #:to-log
+   #:log-opened
+   #:log-closed))
 
 (in-package :cl2nix/log)
+
+(defmethod normalize ((o number))
+  (if (member o '(0 1 2 3 4 5 6 7 8 9))
+      (format nil "0~A" o)
+      (format nil "~A" o)))
 
 (defun time-now ()
   (multiple-value-bind (second minute hour date month year)
       (get-decoded-time)
-    (format nil "~A/~A/~A ~A:~A:~A"
-            date month year hour minute second)))
+    (apply #'format nil "~A/~A/~A ~A:~A:~A"
+           (mapcar #'normalize (list date month year hour minute second)))))
 
-(defvar *log* t)
+(defvar *log* *standard-output*)
 
 (defun open-log (&optional file)
   (if (or (pathnamep file)
@@ -24,8 +32,34 @@
             :if-does-not-exist :create)
       t))
 
-(define-condition log-message (error)
+(defun to-log (condition-class &rest args)
+  (let ((*print-escape* nil))
+    (print-object (apply #'make-condition condition-class args)
+                  *log*)))
+
+(define-condition log-message (simple-condition)
   ((time :initarg :time
          :reader log-time))
   (:default-initargs
    :time (time-now)))
+
+(defmethod log-timestamp ((object log-message))
+  (format nil "~A~0,20T|" (log-time object)))
+
+(define-condition log-opened (log-message)
+  ()
+  (:report (lambda (condition stream)
+             (format stream
+                     "~{~c~}
+~A Log opened"
+                     (make-list 79 :initial-element #\-)
+                     (log-timestamp condition)))))
+
+(define-condition log-closed (log-message)
+  ()
+  (:report (lambda (condition stream)
+             (format stream
+                     "~A Log closed
+~{~c~}"
+                     (log-timestamp condition)
+                     (make-list 79 :initial-element #\-)))))
