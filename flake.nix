e5f7bd6aa15e8ad2fa2141510798527105a87675
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-02-02 11:51:16 +0600

    flake to work after os update

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=0f316e4d72daed659233817ffe52bf08e081b5de";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell {
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ sqlite.out ];
+          buildInputs = with pkgs; [ sbcl sqlite.out ];
+        };
+      }
+    );
+}
